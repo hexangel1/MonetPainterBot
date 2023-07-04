b1b724b5e0be61fb695f3cc591e3f76cd74f21bc
Georgy Artamonov <artamonov2018gi@gmail.com>
2023-07-04 14:23:36 +0300

    fsm context, exception handling added

--- source/telegram_bot.py
+++ source/telegram_bot.py
@@ -1,11 +1,14 @@
 """ Telegram bot endpoints """
 import os
+import sys
 import shutil
 import torch
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import InputFile
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InputFile, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from model import CycleGAN
 
 
@@ -18,39 +21,50 @@ keyboard.add(KeyboardButton('/transfer_style'))
 keyboard.add(KeyboardButton('/help'))
 
 bot = Bot(token=os.environ['TG_BOT_TOKEN'])
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(commands=['start'])
-async def handle_start(msg: types.Message):
+class CustomStates(StatesGroup):
+    bot_started = State()
+    upload_image = State()
+
+
+def clear_userdir(user_id: int):
+    for file in os.listdir(f"storage/{user_id}/"):
+        if os.path.isfile(f"storage/{user_id}/{file}"):
+            os.remove(f"storage/{user_id}/{file}")
+
+
+@dp.message_handler(commands=['start'], state='*')
+async def handle_start(msg: types.Message, state: FSMContext):
     """Processes the start command, creates users' directory and says hi
 
     :param msg: message from user
     :type msg: aiogram.types.Message
     """
-    try:
-        os.mkdir(f"storage/{msg.from_user.id}", mode=0o755)
-    except FileExistsError:
-        pass
+    os.makedirs(f"storage/{msg.from_user.id}/", exist_ok=True, mode=0o755)
+    await CustomStates.bot_started.set()
     await msg.answer("Hi, dear {fname}! 😎\n"
                     "I am MonetStyleBot 🤖\n"
                     "Use /help command to find out what I can do\n".format(fname=msg.from_user.first_name),
                     reply_markup=keyboard)
 
 
-@dp.message_handler(commands=['stop'])
-async def handle_stop(msg: types.Message):
+@dp.message_handler(commands=['stop'], state=CustomStates)
+async def handle_stop(msg: types.Message, state: FSMContext):
     """Processes the stop command, deletes users' directory and says bye
 
     :param msg: message from user
     :type msg: aiogram.types.Message
     """
-    shutil.rmtree(f"storage/{msg.from_user.id}", ignore_errors=True)
-    await msg.answer("Goodbye, dear {fname}".format(fname=msg.from_user.first_name))
+    shutil.rmtree(f"storage/{msg.from_user.id}/", ignore_errors=True)
+    await state.finish()
+    await msg.answer("Goodbye, dear {fname}".format(fname=msg.from_user.first_name),
+                    reply_markup=ReplyKeyboardRemove())
 
 
-@dp.message_handler(commands=['help'])
-async def handle_help(msg: types.Message):
+@dp.message_handler(commands=['help'], state='*')
+async def handle_help(msg: types.Message, state: FSMContext):
     """Processes the help command, sends user commands' descriptions
 
     :param msg: message from user
@@ -65,16 +79,25 @@ async def handle_help(msg: types.Message):
     )
 
 
-@dp.message_handler(content_types=['text'])
-async def handle_text_message(msg: types.Message):
+@dp.message_handler(commands=['transfer_style'], state=CustomStates)
+async def handle_transfer_style(msg: types.Message, state: FSMContext):
+    """Processes the transfer_style command
+
+    :param msg: message from user
+    :type msg: aiogram.types.Message
+    """
+    await CustomStates.upload_image.set()
+    await msg.answer("Send me the image 🖼")
+
+
+@dp.message_handler(content_types=['text'], state=CustomStates)
+async def handle_text_message(msg: types.Message, state: FSMContext):
     """Text handler"""
-    
-    mesg = msg.text[::-1]
-    await msg.answer(mesg)
+    await msg.answer("Sorry, I'm not good at texting yet 😢")
 
 
-@dp.message_handler(content_types=['photo'])
-async def handle_photo_message(msg: types.Message):
+@dp.message_handler(content_types=['photo'], state=CustomStates.upload_image)
+async def handle_photo_message(msg: types.Message, state: FSMContext):
     """Handles photo messages and saves it to the users' directory
 
     :param msg: message from user
@@ -85,15 +108,21 @@ async def handle_photo_message(msg: types.Message):
     downloaded_file = await bot.download_file(file_info.file_path)
     image_path = f"storage/{msg.from_user.id}/{file_ID}"
     styled_image_path = f"storage/{msg.from_user.id}/result.png"
+
     with open(image_path, "wb") as new_file:
         new_file.write(downloaded_file.getvalue())
+    try:
+        gan.transfer_style(image_path, styled_image_path)
+        await bot.send_photo(msg.chat.id, photo=InputFile(styled_image_path), caption="Monet styled photo")
+        clear_userdir(msg.from_user.id)
+    except Exception as error:
+        print("Exception caught:", error, file=sys.stderr, flush=True)
+        await msg.answer("Internal server error")
+    await CustomStates.bot_started.set()
 
-    gan.transfer_style(image_path, styled_image_path)
-    await bot.send_photo(msg.chat.id, photo=InputFile(styled_image_path), caption="Monet styled photo")
 
-
-@dp.message_handler(content_types=['document'])
-async def handle_document_message(msg: types.Message):
+@dp.message_handler(content_types=['document'], state=CustomStates.upload_image)
+async def handle_document_message(msg: types.Message, state: FSMContext):
     """Handles document messages and saves it to the users' directory
 
     :param msg: message from user
@@ -103,6 +132,15 @@ async def handle_document_message(msg: types.Message):
     file_ID = file.file_id
     file_info = await bot.get_file(file_ID)
     downloaded_file = await bot.download_file(file_info.file_path)
-    with open(f"storage/{msg.from_user.id}/{file_ID}={file.file_name}", "wb") as new_file:
+    image_path = f"storage/{msg.from_user.id}/{file_ID}"
+    styled_image_path = f"storage/{msg.from_user.id}/result.png"
+
+    with open(image_path, "wb") as new_file:
         new_file.write(downloaded_file.getvalue())
-    await msg.answer("Document uploaded!")
+    try:
+        gan.transfer_style(image_path, styled_image_path)
+        await bot.send_document(msg.chat.id, document=InputFile(styled_image_path), caption="Monet styled photo")
+    except Exception as error:
+        print("Exception caught:", error, file=sys.stderr, flush=True)
+        await msg.answer("Internal server error")
+    await CustomStates.bot_started.set()
